9e7ed3f402aaf2433fe88f06e9a1cde5eb2ed6b2
saanvi2601 <jigyasachawla13@gmail.com>
2021-05-17 12:43:42 +0530

    test8

--- sketch.js
+++ sketch.js
@@ -93,9 +93,13 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32 && bird.body.speed<1){
+        bird.trajectory = [];
+        Matter.Body.setPosition(bird.body,{x:200,y:50})
        slingshot.attach(bird.body);
+
     }
+    
 }
 
 async function getBackgroundImg(){
@@ -105,7 +109,7 @@ async function getBackgroundImg(){
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=0600 && hour<=1900){
+    if(hour>=06 && hour<=19){
         bg = "sprites/bg1.png";
     }
     else{
